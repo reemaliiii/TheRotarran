3ea3d8f6e7e8705ba26481a23e8f200fc40a63bd
Mohammad Khaled AbouElSherbini <mksherbini@gmail.com>
2020-04-03 14:19:05 +0200

    SimpleMov
    SimpleMov

---
+++ Assets/Scripts/CharacterInteract.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInteract : MonoBehaviour
+{
+    public KeyCode InteractKey = KeyCode.E;
+    public string InteractionTag = "Interactable";
+
+    bool inTrigger;
+    GameObject col;
+    private void Update()
+    {
+        if (inTrigger && col && Input.GetKeyDown(InteractKey))
+        {
+            col.BroadcastMessage("OnInteract");
+
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == InteractionTag)
+        {
+            inTrigger = true;
+            col = collision.gameObject;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == InteractionTag)
+        {
+            inTrigger = false;
+            col = null;
+        }
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float Speed;
+    //CharacterController con;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        //con = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 mov;
+        mov.x = Input.GetAxis("Horizontal");
+        mov.y = Input.GetAxis("Vertical");
+        mov.Normalize();
+
+        mov *= Time.deltaTime * Speed;
+
+        rb.MovePosition(rb.position + mov);
+        //con.Move(mov);
+    }
+}
