6988e7def3aeadd6dd790070ba6a60e919e89a06
Amjad Mohamed <amjadmohamed55@gmail.com>
2020-04-04 04:29:08 +0200

    Added dialogue mechanic.
    Added dialogue mechanics where the player can interact with other characters.

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    Queue<string> sentences;
+
+    public Text dialogue;
+    public GameObject dialogueBox;
+
+    public Animator anim;
+
+    private void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        dialogueBox.SetActive(true);
+        sentences.Clear();
+        anim.SetBool("IsOpen", true);
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        NextSentence();
+
+        // seems like there is a bug here where even if i commented this line i can still skip the dialogue with "Enter" button 
+        /*if (Input.GetKeyDown(KeyCode.Return))
+        {
+            NextSentence();
+            Debug.Log("ss");
+        }*/
+
+    }
+
+    public void NextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(WordByWordSentence(sentence));
+    }
+
+    IEnumerator WordByWordSentence(string sentence)
+    {
+        dialogue.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogue.text += letter;
+            yield return null;
+        }
+    }
+
+    private void EndDialogue()
+    {
+        dialogueBox.SetActive(false);
+        anim.SetBool("IsOpen", false);
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    DialogueTrigger DT;
+    public float movementSpeed = 10;
+    Vector2 dir;
+    private GameObject Character;
+    public GameObject InteractionText;
+
+    // counter to stop the bug where you can reset the dialogue even before finishing it.
+    private int cnt = 1;
+
+
+    void Update()
+    {
+        dir.x = Input.GetAxis("Horizontal");
+        dir.y = Input.GetAxis("Vertical");
+
+        transform.Translate(dir * movementSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.transform.tag == "Char")
+        {
+            InteractionText.SetActive(true);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.transform.tag == "Char" && Input.GetKeyDown(KeyCode.T) && cnt == 1)
+        {
+            Character = collision.gameObject; 
+            Debug.Log("convo started");
+            Character.GetComponent<DialogueTrigger>().TriggerDialogue();
+            cnt = 0;
+            
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Char")
+        {
+            InteractionText.SetActive(false);
+            cnt = 1;
+        }
+    }
+
+}
