e24123363a411bced5a065cb4f8074c598a56621
Mohammad Khaled AbouElSherbini <mksherbini@gmail.com>
2020-04-06 17:19:38 +0200

    Fix boss kill, Credits

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -135,8 +135,8 @@ public class DialogueManager : MonoBehaviour
     public void OnKillButtonClick(Transform chr)
     {
         //EndDialogue();
-        ShouldKill = chr.GetComponent<DialogueTrigger>().ShouldKill;
-        chr.GetComponentInChildren<Animator>().SetBool("IsDead", true);
+        DialogueTrigger dt = chr.GetComponent<DialogueTrigger>();
+        ShouldKill = dt.ShouldKill;
         if (ShouldKill != true)
         {
             KilledWrongPerson++;
@@ -151,6 +151,11 @@ public class DialogueManager : MonoBehaviour
         }
         SpokenTo.Add(chr);
         GameManager.instace.UpdateKeysScore(KilledRightPerson);
+        if (dt.IsBoss)
+        {
+            dt.TriggerDialogue();
+        }
+        chr.GetComponentInChildren<Animator>().SetBool("IsDead", true);
         Debug.Log("KilledWrongPerson: " + KilledWrongPerson + " KilledRightPerson: " + KilledRightPerson);
     }
 
--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -2,44 +2,59 @@
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
 
 public class MainMenu : Menu
 {
     [SerializeField]
     private Button m_PlayButton;
     [SerializeField]
-    private Button m_OptionsButton;
+    private Button m_Credits;
+    [SerializeField]
+    private GameObject m_CreditsMenu;
+    //private Button m_OptionsButton;
     [SerializeField]
     private Button m_ExitButton;
-    [SerializeField]
-    private OptionsMenu m_OptionsMenu;
+    //[SerializeField]
+    //private OptionsMenu m_OptionsMenu;
 
-    protected override void Awake() {
+    protected override void Awake()
+    {
         base.Awake();
 
         m_PlayButton.onClick.AddListener(OnPlayButtonClick);
-        m_OptionsButton.onClick.AddListener(OnOptionsButtonClick);
+        //m_OptionsButton.onClick.AddListener(OnOptionsButtonClick);
         m_ExitButton.onClick.AddListener(OnExitButtonClick);
+        m_Credits.onClick.AddListener(OnCreditButtonClick);
 
         //Application.quitting += Application_OnQuit;
     }
 
-    private void OnPlayButtonClick() {
+    private void OnCreditButtonClick()
+    {
+        m_CreditsMenu.SetActive(true);
+        Hide();
+    }
+
+    private void OnPlayButtonClick()
+    {
         SceneManager.LoadScene(1);
         Hide();
     }
 
-    private void OnOptionsButtonClick() {
-        // show options menu
-        Hide();
-        m_OptionsMenu.Show();
-    }
+    //private void OnOptionsButtonClick() {
+    //    // show options menu
+    //    Hide();
+    //    m_OptionsMenu.Show();
+    //}
 
-    private void OnExitButtonClick() {
+    private void OnExitButtonClick()
+    {
         Application.Quit();
     }
 
-    private void Application_OnQuit() {
+    private void Application_OnQuit()
+    {
         // optional; save progress of the game
     }
 }
